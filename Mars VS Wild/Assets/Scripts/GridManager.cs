393485faf5ac795939faa74d26461c5f45daf106
Pierre Ferrari <pierreferrari22@gmail.com>
2022-03-04 22:50:59 +0100

    Grid & tiles v1

--- Mars VS Wild/Assets/Scripts/GridManager.cs
+++ Mars VS Wild/Assets/Scripts/GridManager.cs
@@ -16,20 +16,23 @@ public class GridManager : MonoBehaviour
     void GenerateGrid() 
     {
         _tiles = new Dictionary<Vector2, Tile>();
-        for (int x = 0; x < _width; x++) {
-            for (int y = 0; y < _height; y++) {
+        
+        for (int x = 0; x < _width; x++) 
+        {
+            for (int y = 0; y < _height; y++) 
+            {
                 var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
  
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
- 
- 
+                
                 _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
  
         _cam.transform.position = new Vector3((float)_width/2 -0.5f, (float)_height / 2 - 0.5f,-10);
+        _cam.transform.position = new Vector3(3.6f, _cam.transform.position.y, _cam.transform.position.z);
     }
  
     public Tile GetTileAtPosition(Vector2 pos) {
