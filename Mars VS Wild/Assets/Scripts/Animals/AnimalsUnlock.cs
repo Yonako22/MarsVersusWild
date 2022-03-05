58cc3930300004cc55c90acce9e3e937bb1229b9
N-Brasier <nbrasier@rubika-edu.com>
2022-03-05 15:05:54 +0100

    création folder scripts animaux

---
+++ Mars VS Wild/Assets/Scripts/Animals/AnimalsUnlock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalsUnlock : MonoBehaviour
+{
+
+public 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
