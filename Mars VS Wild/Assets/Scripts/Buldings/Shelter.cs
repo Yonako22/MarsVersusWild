d8e2e1e2dbf44947b5cca07c24aa11a09d40a691
Terri <a.dubuc@rubika-edu.com>
2022-03-05 23:22:01 +0100

    Oui

--- Mars VS Wild/Assets/Scripts/Buldings/Shelter.cs
+++ Mars VS Wild/Assets/Scripts/Buldings/Shelter.cs
@@ -1,13 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shelter : Buildings
 {
 
     public float shelterHealth = 100f;
-    
+    public Slider healthBar;
+    private void Start()
+    {
+        buildingHP = 100;
+    }
+
     void Update()
     {
+        
+        
         if (buildingLevel == 2)
         {
            GameManager.instance.woodPerSecond = 2;
