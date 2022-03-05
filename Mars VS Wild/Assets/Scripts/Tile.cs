59964fa38d8548f46a01593d6160b103c4716ce8
Pierre Ferrari <pierreferrari22@gmail.com>
2022-03-06 00:47:28 +0100

    fix buildings

--- Mars VS Wild/Assets/Scripts/Tile.cs
+++ Mars VS Wild/Assets/Scripts/Tile.cs
@@ -1,3 +1,4 @@
+using Manager;
 using Unity.Mathematics;
 using UnityEngine;
  
@@ -20,32 +21,35 @@ public class Tile : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (building != null)
+        if (!UIManager.instance.isPaused)
         {
-            buildingScript = building.GetComponent<Buildings>();  
-            if(!buildingScript.canBeUpgrade)
+            if (building != null)
             {
-                Debug.Log("Impossible de construire ici, il y a déjà une tourelle.");
-                return;
-            }
+                buildingScript = building.GetComponent<Buildings>();  
+                if(!buildingScript.canBeUpgrade)
+                {
+                    Debug.Log("Impossible de construire ici, il y a déjà une tourelle.");
+                    return;
+                }
                                                                         
-            if (buildingScript.canBeUpgrade)
-            {
-                buildingScript.buildingLevel +=1;
-            }
-        }
-        else
-        {
-            GameObject buildingToBuild = BuildingManager.instance.GetBuildingToBuild();
-            
-            if (GameManager.instance.wood >= buildingToBuild.GetComponent<Buildings>().buildingPriceLvl1)
-            {
-                building = (GameObject)Instantiate(buildingToBuild, transform.position, quaternion.identity);
-                GameManager.instance.wood -= buildingToBuild.GetComponent<Buildings>().buildingPriceLvl1;
+                if (buildingScript.canBeUpgrade)
+                {
+                    buildingScript.buildingLevel +=1;
+                }
             }
             else
             {
-                Debug.Log("pas assez d'argent");
+                GameObject buildingToBuild = BuildingManager.instance.GetBuildingToBuild();
+            
+                if (GameManager.instance.wood >= buildingToBuild.GetComponent<Buildings>().buildingPriceLvl1)
+                {
+                    building = (GameObject)Instantiate(buildingToBuild, transform.position, quaternion.identity);
+                    GameManager.instance.wood -= buildingToBuild.GetComponent<Buildings>().buildingPriceLvl1;
+                }
+                else
+                {
+                    Debug.Log("pas assez d'argent");
+                }
             }
         }
     }
