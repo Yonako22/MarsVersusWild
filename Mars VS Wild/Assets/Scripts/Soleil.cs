ae9f9602d30001892374cefe36e563fa6f5dda68
Pierre Ferrari <pierreferrari22@gmail.com>
2022-03-05 17:06:06 +0100

    jour nuit claqué

--- Mars VS Wild/Assets/Scripts/Soleil.cs
+++ Mars VS Wild/Assets/Scripts/Soleil.cs
@@ -1,18 +1,39 @@
+/*
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Soleil : MonoBehaviour
 {
     [SerializeField] private Light _light;
-    [SerializeField] private AnimationCurve _curveX;
-    [SerializeField] private AnimationCurve _curveY;
 
-    void Start()
+    public bool itsMorning,itsAfterrnoon,itsEvening,itsNight;
+
+    private void Start()
     {
-        
+        StartCoroutine(cycleJourNuit());
     }
 
-    void Update()
+    IEnumerator cycleJourNuit()
     {
-        
+        itsEvening = false;
+        itsMorning = true;
+        yield return new WaitForSeconds(60f);
+        itsMorning = false;
+        itsAfterrnoon = true;
+        yield return new WaitForSeconds(60f);
+        itsAfterrnoon = false;
+        itsEvening = true;
+        yield return new WaitForSeconds(60f);
+        itsEvening = false;
+        itsNight = true;
+        yield return new WaitForSeconds(60f);
+        StartCoroutine(cycleJourNuit());
+    }
+    
+    private void Update()
+    {
+        Debug.Log();
     }
 }
+*/
