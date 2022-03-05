1365f2498dca0011ccdf8e4b317c013bc1010bcc
Pierre Ferrari <pierreferrari22@gmail.com>
2022-03-05 16:35:55 +0100

    pleins de trucs

--- Mars VS Wild/Assets/Scripts/Buldings/Shelter.cs
+++ Mars VS Wild/Assets/Scripts/Buldings/Shelter.cs
@@ -1,21 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shelter : Buildings
 {
-    public BuildingManager buildingManager;
-    
     void Update()
     {
         if (buildingLevel == 2)
         {
-            buildingManager.woodPerSecond = 2;
+           GameManager.instance.woodPerSecond = 2;
         }
         if (buildingLevel == 3)
         {
-            buildingManager.woodPerSecond = 2.5f;
+            GameManager.instance.woodPerSecond = 2.5f;
             canBeUpgrade = false;
         }
 
