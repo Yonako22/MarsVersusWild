a8e2756a09199496f9fcae9e8cbc132b58a5de0f
N-Brasier <nbrasier@rubika-edu.com>
2022-03-05 16:28:32 +0100

    script animal unlock sans lien menu

--- Mars VS Wild/Assets/Scripts/Animals/AnimalsUnlock.cs
+++ Mars VS Wild/Assets/Scripts/Animals/AnimalsUnlock.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AnimalsUnlock : MonoBehaviour
 {
@@ -14,17 +16,14 @@ public class AnimalsUnlock : MonoBehaviour
     public GameObject gorilla;
     public GameObject rhino;
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
         if (gameManager.totalTime > 45 && !giraffeUnlocked)
         {
             placeToSpawn = Random.Range(0, 14);
             Instantiate(giraffe, enemiesSpawn.spawns[placeToSpawn].transform.position, Quaternion.identity);
+            giraffe.GetComponent<GirafeAttack>().enabled = false;
+            giraffe.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 150);
             giraffeUnlocked = true;
         }
         
@@ -32,6 +31,8 @@ public class AnimalsUnlock : MonoBehaviour
         {
             placeToSpawn = Random.Range(0, 14);
             Instantiate(gorilla, enemiesSpawn.spawns[placeToSpawn].transform.position, Quaternion.identity);
+            gorilla.GetComponent<GorilleAttack>().enabled = false;
+            gorilla.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 150);
             gorillaUnlocked = true;
         }
         
@@ -39,7 +40,27 @@ public class AnimalsUnlock : MonoBehaviour
         {
             placeToSpawn = Random.Range(0, 14);
             Instantiate(rhino, enemiesSpawn.spawns[placeToSpawn].transform.position, Quaternion.identity);
+            rhino.GetComponent<RhinoAttack>().enabled = false;
+            rhino.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 150);
             gorillaUnlocked = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (giraffeUnlocked && col.gameObject.CompareTag("Shelter"))
+        {
+            //Déverouiller la girafe dans le menu puis détruire le gameobject
+        }
+        
+        if (gorillaUnlocked && col.gameObject.CompareTag("Shelter"))
+        {
+            //Déverouiller le gorille dans le menu puis détruire le gameobject
+        }
+        
+        if (rhinoUnlocked && col.gameObject.CompareTag("Shelter"))
+        {
+            //Déverouiller le gorille dans le menu puis détruire le gameobject
         }
     }
 }
